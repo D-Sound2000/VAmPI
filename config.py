2b94d1c92a73f164837fbd2cc94405e11faf1334
D-Sound2000 <DhruvSoundar@gmail.com>
2026-08-08 12:09:13 -0700

    use a strong random JWT signing key

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 import connexion
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -10,7 +11,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(vuln_app.app.root_path, 'd
 vuln_app.app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 vuln_app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-vuln_app.app.config['SECRET_KEY'] = 'random'
+vuln_app.app.config['SECRET_KEY'] = secrets.token_urlsafe(64)
 # start the db
 db = SQLAlchemy(vuln_app.app)
 
